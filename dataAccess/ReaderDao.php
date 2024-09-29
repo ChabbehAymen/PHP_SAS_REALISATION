3281c24431f79a65ec351953a12461cfa676e4ce
ayoubAmazu <ayoubamazu@gmail.com>
2024-09-29 18:53:03 +0100

    pesentation web half done

--- dataAccess/ReaderDao.php
+++ dataAccess/ReaderDao.php
@@ -3,7 +3,11 @@ require_once dirname(dirname(__FILE__))."/dataAccess/DataAccess.php";
 require_once dirname(dirname(__FILE__))."/entities/Reader.php";
 class ReaderDao extends DataAccess
 {
+ 
     function __construct() {
         parent::construct('readers');
+        
     }
+
+
 }
--- services/ReadersService.php
+++ services/ReadersService.php
@@ -1,10 +1,28 @@
 <?php
 require_once(dirname(dirname(__FILE__)) . "/services/Service.php");
+require_once(dirname(dirname(__FILE__)) . "/services/BooksService.php");
 require_once(dirname(dirname(__FILE__)) . "/dataAccess/ReaderDao.php");
 
 class ReadersService extends Service
 {
+   private $books;
+   private $loans;
+
     public function __construct() {
         parent::__construct(new ReaderDao());
+        $this->books = new BookService();
+        $this->loans = new LoanService();
+
+    }
+
+    public function bookList()
+    {
+     
+            return $this->books->getAll();
+    }
+    public function loanList()
+    {
+     
+            return $this->loans->getAll();
     }
 }
